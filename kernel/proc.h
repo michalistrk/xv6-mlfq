18732fbdd4e9977474926a527afe26f96c81d56e
s3gfault <miketrik07@gmail.com>
2026-01-05 18:14:04 +0200

    extend struct proc with MLFQ fields

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  //MLFQ related fields
+  int level;
+  int ticks_used;
+  int wait_ticks;
 };
